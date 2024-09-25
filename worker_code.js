d3fddb0ffc1f6644bdae27f33a672a8d41766be8
Vitaly Ford <5395250+vitalyford@users.noreply.github.com>
2024-09-25 12:03:54 -0400

    Add a sample of cloudflare worker code

---
+++ worker_code.js
@@ -0,0 +1,53 @@
+export default {
+    async fetch(request, env) {
+      if (request.method === 'OPTIONS') {
+        return new Response(null, {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
+        });
+      }
+  
+      try {
+        const { prompt } = await request.json();
+  
+        if (!prompt) {
+          return new Response('Prompt is required', {
+            status: 400,
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+              'Access-Control-Allow-Headers': 'Content-Type',
+            },
+          });
+        }
+  
+        const inputs = { prompt };
+  
+        const response = await env.AI.run(
+          '@cf/stabilityai/stable-diffusion-xl-base-1.0',
+          inputs,
+        );
+  
+        return new Response(response, {
+          headers: {
+            'content-type': 'image/png',
+            'Access-Control-Allow-Origin': '*', // Allow all origins
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Allow specific methods
+            'Access-Control-Allow-Headers': 'Content-Type', // Allow specific headers
+          },
+        });
+      } catch (error) {
+        return new Response('Error processing request ' + error, {
+          status: 500,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+          },
+        });
+      }
+    },
+  };
